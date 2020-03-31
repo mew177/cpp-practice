ba9e9b8fde9672ce8d89efefc3d4f25a384b7406
Wu, Meng Ju <mew177@pitt.edu>
2020-03-31 10:15:35 -0400

    1062

---
+++ leetcode/1062 - Longest Repeating Substring/1062 - Longest Repeating Substring/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  1062 - Longest Repeating Substring
+//
+//  Created by Wu, Meng Ju on 2020/3/30.
+//  Copyright © 2020 Pitt. All rights reserved.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
