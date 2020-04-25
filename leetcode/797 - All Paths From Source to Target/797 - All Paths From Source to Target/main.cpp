f8b9333558d3dc37edf39d5a7f975951544272a9
Wu, Meng Ju <mew177@pitt.edu>
2020-04-25 08:07:41 +0800

    797

---
+++ leetcode/797 - All Paths From Source to Target/797 - All Paths From Source to Target/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  797 - All Paths From Source to Target
+//
+//  Created by Wu, Meng Ju on 2020/4/25.
+//  Copyright © 2020 Pitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
